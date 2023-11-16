6a9ab132838b9bb1bc228bc4838b82471f77aa23
minutogit <sgalek@gmx.de>
2023-11-16 18:18:20 +0100

    unterschreiben der der buergen funktioniert

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import os
 import hashlib
 import binascii
+import base64
+
 import base58
 from ecdsa import SigningKey, SECP256k1
 from mnemonic import Mnemonic
@@ -75,6 +77,7 @@ class Key:
 
 class Person:
     def __init__(self, key: Key, name, address, gender, email, phone, service_offer, coordinates, validity):
+        self.key = key
         self.id = key.id
         self.name = name
         self.address = address
@@ -83,6 +86,31 @@ class Person:
         self.phone = phone
         self.service_offer = service_offer  # Angebot / Fähigkeiten
         self.coordinates = coordinates
+
+    def create_voucher(self, amount, region, validity):
+        """ Erstellt einen neuen MinutoVoucher. """
+        return MinutoVoucher(self, amount, region, validity)
+
+    def sign_voucher_as_guarantor(self, voucher):
+        """ Bürgen signieren den Gutschein mit erweiterten Informationen. """
+        guarantor_info = {
+            "id": self.id,
+            "name": self.name,
+            "address": self.address,
+            "gender": self.gender,
+            "email": self.email,
+            "phone": self.phone,
+            "coordinates": self.coordinates,
+            "signature_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+        signature_data = voucher.get_voucher_data() + json.dumps(guarantor_info, sort_keys=True)
+        signature = self.key.sign_message(self.key.private_key, signature_data)
+        voucher.guarantor_signatures.append((guarantor_info, signature))
+
+    def sign_voucher_as_creator(self, voucher):
+        # Schöpfer signiert den Gutschein, inklusive der Bürgen-Signaturen
+        data_to_sign = voucher.get_voucher_data(include_guarantor_signatures=True)
+        voucher.creator_signature = self.key.sign_message(self.key.private_key, data_to_sign)
 
     def __str__(self):
         return f"Person({self.id}, {self.name}, {self.address}, {self.gender}, {self.email}, {self.phone}, {self.service_offer}, {self.coordinates})"
@@ -104,27 +132,6 @@ class MinutoVoucher:
         self.creation_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.guarantor_signatures = []  # Signaturen der Bürgen
         self.creator_signature = None  # Signatur des Schöpfers
-
-    def sign_voucher_as_guarantor(self, guarantor_id, private_key, guarantor_name, guarantor_address, guarantor_gender, guarantor_email, guarantor_phone, guarantor_coordinates):
-        # Bürgen signieren den Gutschein mit erweiterten Informationen
-        guarantor_info = {
-            "id": guarantor_id,
-            "name": guarantor_name,
-            "address": guarantor_address,
-            "gender": guarantor_gender,
-            "email": guarantor_email,
-            "phone": guarantor_phone,
-            "coordinates": guarantor_coordinates,
-            "signature_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        }
-        signature_data = self.get_voucher_data() + json.dumps(guarantor_info, sort_keys=True)
-        signature = self.creator.key.sign_message(private_key, signature_data)
-        self.guarantor_signatures.append((guarantor_id, signature))
-
-    def sign_voucher_as_creator(self, private_key):
-        # Schöpfer signiert den Gutschein, inklusive der Bürgen-Signaturen
-        data_to_sign = self.get_voucher_data(include_guarantor_signatures=True)
-        self.creator_signature = self.creator.key.sign_message(private_key, data_to_sign)
 
     def get_voucher_data(self, include_guarantor_signatures=False):
         # Dynamische Generierung der Daten, inklusive optionaler Bürgen-Signaturen
@@ -149,9 +156,19 @@ class MinutoVoucher:
         return len(self.guarantor_signatures) >= 2 and self.creator_signature is not None
 
     def save_to_disk(self, file_path):
-        # Speichert den Gutschein als Datei
+        # Konvertiert Byte-Objekte in hexadezimale Strings für die Speicherung
+        if self.creator_signature:
+            self.creator_signature = base64.b64encode(self.creator_signature).decode()
+        self.guarantor_signatures = [(g[0], base64.b64encode(g[1]).decode()) for g in self.guarantor_signatures]
+
         with open(file_path, 'w') as file:
             file.write(json.dumps(self.__dict__, sort_keys=True, indent=4))
+
+        # Konvertiert die Signaturen zurück in Bytes, um den Zustand des Objekts beizubehalten
+        if self.creator_signature:
+            self.creator_signature = base64.b64decode(self.creator_signature)
+        self.guarantor_signatures = [(g[0], base64.b64decode(g[1])) for g in self.guarantor_signatures]
+
 
     @classmethod
     def read_from_disk(cls, file_path):
@@ -180,8 +197,13 @@ class MinutoVoucher:
             )
             voucher.voucher_id = data.get('voucher_id', str(uuid.uuid4()))
             voucher.creation_date = data.get('creation_date', datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-            voucher.guarantor_signatures = data.get('guarantor_signatures', [])
-            voucher.creator_signature = data.get('creator_signature')
+
+            # Konvertiert die hexadezimalen Strings zurück in Byte-Objekte
+            voucher.creator_signature = base64.b64decode(data.get('creator_signature', '')) if data.get(
+                'creator_signature') else None
+            voucher.guarantor_signatures = [(g[0], base64.b64decode(g[1])) for g in
+                                            data.get('guarantor_signatures', [])]
+
             return voucher
 
     def __str__(self):
@@ -217,10 +239,12 @@ buerge_weiblich = Person(Key("rookie era bamboo industry group furnace axis diso
 buerge_maennlich = Person(Key("strong symptom minor attract math clock pool elite half guess album close"), "Max Mustermann", "Musterstraße 1", 1, "max@example.com", "0123456789", "IT-Support", "50.1109, 8.6821", "2023-12-31")
 
 # Erstelle einen MinutoVoucher mit dieser Person als Ersteller
-voucher = MinutoVoucher(hansdampf, 100, "Frankfurt", "2028")
+voucher = hansdampf.create_voucher(100, "Frankfurt", "2028")
 voucher.save_to_disk("minutoschein.txt")
 
 b1_load_voucher = MinutoVoucher.read_from_disk("minutoschein.txt")
+buerge_maennlich.sign_voucher_as_guarantor(b1_load_voucher)
+b1_load_voucher.save_to_disk("minutoschein-b1.txt")
 
 print(voucher)
 print(b1_load_voucher)
