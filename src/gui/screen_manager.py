d8f77d77709f58b2cd7bd2716ffa2a8cbe5c0695
minutogit <sgalek@gmx.de>
2023-12-06 10:35:17 +0100

    gui start tests

---
+++ src/gui/screen_manager.py
@@ -0,0 +1,49 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+# for hot reload
+from kivymd.app import MDApp
+#from kivymd.tools.hotreload.app import MDApp
+
+
+Builder.load_file('gui_layout.kv')
+
+# Definiere die verschiedenen Bildschirme
+class DashboardScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+    pass
+
+class CreateVoucherScreen(Screen):
+    pass
+
+class PerformTransactionScreen(Screen):
+    pass
+
+class ReceiveTransactionScreen(Screen):
+    pass
+
+class VoucherListScreen(Screen):
+    pass
+
+# Hauptanwendung
+class MyApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "Blue"  # Setze das Farbschema
+
+        # Erstelle den Screen Manager
+        sm = ScreenManager()
+        sm.add_widget(DashboardScreen(name='dashboard'))
+        sm.add_widget(SettingsScreen(name='settings'))
+        sm.add_widget(CreateVoucherScreen(name='create_voucher'))
+        sm.add_widget(PerformTransactionScreen(name='perform_transaction'))
+        sm.add_widget(ReceiveTransactionScreen(name='receive_transaction'))
+        sm.add_widget(VoucherListScreen(name='voucher_list'))
+
+        sm.current = 'dashboard'
+        return sm
+
+if __name__ == '__main__':
+    MyApp().run()
