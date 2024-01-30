9b7776fe01d0d77ce8381ec925b01ba12606cd59
Sebastian <sgalek@gmx.de>
2024-01-30 21:40:58 +0100

    gui fixes / improvements

--- src/gui/qt/qt_main.py
+++ src/gui/qt/qt_main.py
@@ -40,12 +40,10 @@ def open_data_file():
 
     if file_path:
         voucher, info_msg = user_profile.open_file(file_path)
-        dialog_voucher_list.init_values()
+        win['dialog_voucher_list'].init_values()
         show_message_box("Info", info_msg)
         if voucher is not None:
-            form_show_voucher.show_voucher(voucher)
-
-
+            win['form_show_voucher'].show_voucher(voucher)
 
 
 class FormSignAsGuarantor(QMainWindow, Ui_FormSignVoucherAsGuarantor):
@@ -82,16 +80,18 @@ class FormSignAsGuarantor(QMainWindow, Ui_FormSignVoucherAsGuarantor):
     def sign_as_guarantor(self):
         # if sign successful
         if user_profile.person.sign_voucher_as_guarantor(self.voucher):
-            self.guarantor_signature = user_profile.person.get_own_guarantor_signature(self.voucher)
+            user_profile.save_file(self.voucher)  # save signed voucher
+            # update gui
             self.checkBox_liability_acceptance.setEnabled(False)
             self.pushButton_sign_as_guarantor.setEnabled(False)
             self.pushButton_save_as_file.setEnabled(True)
+            win['form_show_voucher'].show_voucher(self.voucher)  # reload voucher gui
 
     def save_signature_as_file(self):
         """
         Save the signature as a file, either encrypted or plain based on the user's choice.
         """
-
+        self.guarantor_signature = user_profile.person.get_own_guarantor_signature(self.voucher)
         creator_id = self.voucher.creator_id
         creator_name = self.voucher.creator_first_name
 
@@ -111,24 +111,40 @@ class FormSignAsGuarantor(QMainWindow, Ui_FormSignVoucherAsGuarantor):
             user_profile._secure_file_handler.encrypt_with_shared_secret_and_save(
                 self.guarantor_signature, filename_with_path, creator_id, user_profile.person.id)
 
-
-
     def show_form(self, voucher):
         self.voucher = voucher
         self.guarantor_signature = None
         self.pushButton_sign_as_guarantor.setEnabled(False)
         self.pushButton_save_as_file.setEnabled(False)
         self.pushButton_send_as_email.setEnabled(False)
-        self.checkBox_liability_acceptance.setChecked(False)
-        self.setWindowTitle(f"eMinuto als Bürge unterschreiben - Profil: {user_profile.profile_name}")
-        self.show()
+        self.pushButton_save_as_file.setEnabled(False)
 
+        self.checkBox_liability_acceptance.setChecked(False)
+        self.checkBox_liability_acceptance.setEnabled(True)
+        self.setWindowTitle(f"eMinuto als Bürge unterschreiben - Profil: {user_profile.profile_name}")
+
+        # when already own signate then change gui
+        for sig in voucher.guarantor_signatures:
+            if sig[0]["id"] == user_profile.person.id:
+                self.checkBox_liability_acceptance.setChecked(True)
+                self.checkBox_liability_acceptance.setEnabled(False)
+                self.pushButton_sign_as_guarantor.setEnabled(False)
+                self.pushButton_save_as_file.setEnabled(True)
+                self.show()
+                show_message_box("Unterschrift vorhanden",
+                                 "Dieser Gutschein ist bereits unterschrieben. Die Unterschrift einfach dem Ersteller "
+                                 "zukommen lassen.")
+                return
+
+        self.show()
+        self.raise_()
 
 
 class FormSendToGuarantor(QMainWindow, Ui_FormSendToGuarantor):
     """
     A window form to send a voucher for signing to a guarantor or save the voucher as a file for obtaining a signature.
     """
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -194,16 +210,12 @@ class FormSendToGuarantor(QMainWindow, Ui_FormSendToGuarantor):
                 # Save the voucher unencrypted
                 user_profile.person.save_voucher(filename=filename_with_path, voucher=self.voucher)
 
-
     def show_form(self, voucher):
         self.voucher = voucher
         self.lineEdit_user_ID.setText(user_profile.person.id)
         self.setWindowTitle(f"eMinuto an Bürge senden - Profil: {user_profile.profile_name}")
         self.show()
-
-
-
-
+        self.raise_()
 
 
 class FormShowRawData(QMainWindow, Ui_FormShowRawData):
@@ -219,6 +231,7 @@ class FormShowRawData(QMainWindow, Ui_FormShowRawData):
         self.textEdit_text_data.setText(json_representation)
         self.setWindowTitle(f"eMinuto Rohdaten - Profil: {user_profile.profile_name}")
         self.show()
+        self.raise_()
 
 
 class FormShowVoucher(QMainWindow, Ui_FormShowVoucher):
@@ -254,30 +267,32 @@ class FormShowVoucher(QMainWindow, Ui_FormShowVoucher):
             button.setStyleSheet(button_stylesheet)
 
     def sign_as_creator(self):
-        sign = show_yes_no_box("Gutschein selbst unterschreiben", "Soll der Gutschein unterschrieben werden? Damit ist er gültig und kann für Transaktionen verwenden werden.")
+        sign = show_yes_no_box("Gutschein selbst unterschreiben",
+                               "Soll der Gutschein unterschrieben werden? Damit ist er gültig und kann für Transaktionen verwenden werden.")
         if sign:
             signed, message = user_profile.person.sign_voucher_as_creator(self.voucher)
 
             if signed:
                 user_profile.save_file(self.voucher)
                 user_profile.person.current_voucher = None
-                self.show_voucher(self.voucher) # to update all values
-                show_message_box("Unterschrift erfolgreich", "Unterschrift war erfolgreich. Der Gutschein ist nun fertig und kann verwendet werden.")
+                self.show_voucher(self.voucher)  # to update all values
+                show_message_box("Unterschrift erfolgreich",
+                                 "Unterschrift war erfolgreich. Der Gutschein ist nun fertig und kann verwendet werden.")
 
             else:
                 show_message_box("Fehler", f"Unterschrift fehlgeschlagen. {message}")
 
-
     def sing_as_guarantor(self):
         # check if gender is set
-        sign_voucher = (user_profile.person.gender != 0)
-        if not sign_voucher:
-            sign_voucher = show_yes_no_box("Geschlecht fehlt", "Im Profil muss das Geschlecht angegeben werden. Dennoch unterschreiben? (nicht empfohlen)")
-        if sign_voucher:
-            form_sing_as_guarantor.show_form(self.voucher)
+        gender_is_set = (user_profile.person.gender != 0)
+        if not gender_is_set:
+            gender_is_set = show_yes_no_box("Geschlecht fehlt",
+                                            "Im Profil muss das Geschlecht angegeben werden. Dennoch unterschreiben? (nicht empfohlen)")
+        if gender_is_set:
+            win['form_sign_as_guarantor'].show_form(self.voucher)
 
     def send_to_guarantor(self):
-        form_send_to_guarantor.show_form(self.voucher)
+        win['form_send_to_guarantor'].show_form(self.voucher)
 
     def update_status(self, voucher: MinutoVoucher):
         """
@@ -286,7 +301,7 @@ class FormShowVoucher(QMainWindow, Ui_FormShowVoucher):
         """
         # Check conditions for voucher status
         own_voucher = (voucher.creator_id == user_profile.person.id)
-        number_of_guarantors =  len(voucher.guarantor_signatures)
+        number_of_guarantors = len(voucher.guarantor_signatures)
         enough_guarantors = (number_of_guarantors >= 2)
 
         no_creator_signature = (voucher.creator_signature is None)
@@ -297,7 +312,8 @@ class FormShowVoucher(QMainWindow, Ui_FormShowVoucher):
 
         # Enable or disable buttons based on voucher status
         self.pushButtonSendToGuarantor.setEnabled(own_voucher and not enough_guarantors)
-        self.pushButtonSignAsCreator.setEnabled(own_voucher and enough_guarantors and both_genders_present and no_creator_signature)
+        self.pushButtonSignAsCreator.setEnabled(
+            own_voucher and enough_guarantors and both_genders_present and no_creator_signature)
         self.pushButtonSignAsGuarantor.setEnabled(not own_voucher and not enough_guarantors)
 
         voucher_status = ""
@@ -335,7 +351,7 @@ class FormShowVoucher(QMainWindow, Ui_FormShowVoucher):
 
     def show_raw_data(self):
         """ Display raw data of the voucher. """
-        form_show_raw_data.show_data(self.voucher)
+        win['form_show_raw_data'].show_data(self.voucher)
 
     def init_table(self, voucher: MinutoVoucher = None):
         if voucher is None:
@@ -404,10 +420,15 @@ class FormShowVoucher(QMainWindow, Ui_FormShowVoucher):
         """
         self.voucher = voucher
         self.init_table(voucher)
-
         self.update_status(voucher)
         self.setWindowTitle(f"eMinuto Details - Profil: {user_profile.profile_name}")
         self.show()
+        self.raise_()
+
+        # if ready to sign as creator show message
+        if self.pushButtonSignAsCreator.isEnabled():
+            show_message_box("Gutschein mit Unterschrift vervollständigen",
+                             "Unterschreibe diesen fast fertigen Gutschein damit dieser genutzt werden kann.")
 
     def create_non_editable_item(self, text):
         """ Create a non-editable table item with the provided text. """
@@ -425,12 +446,15 @@ class Dialog_Create_Minuto(QMainWindow, Ui_DialogCreateMinuto):
         self.pushButton_CreateVoucher.clicked.connect(self.create_minuto)
         self.init_values()
 
+    # initialized flag is need to prevent reset to init_values when windows was closed
+    # so form data does ntt get lost on window close
     def showEvent(self, event: QShowEvent):
         """Called when the dialog is shown."""
         if not self.initialized:
             self.init_values()
             self.initialized = True
         super().showEvent(event)
+        self.raise_()
 
     def create_minuto(self):
         # Extrahiere die Werte aus den GUI-Elementen
@@ -448,15 +472,14 @@ class Dialog_Create_Minuto(QMainWindow, Ui_DialogCreateMinuto):
         coordinates = self.lineEdit_coordinates.text()
         is_test_vocher = self.checkBox_is_test_voucher.isChecked()
 
-        gender = self.comboBox_creator_gender.currentIndex() # 0 unknown, 1 male, 2 female
+        gender = self.comboBox_creator_gender.currentIndex()  # 0 unknown, 1 male, 2 female
         footnote = self.label_footnote.text()
         description = self.label_voucher_description.text()
         voucher = user_profile.create_voucher(first_name, last_name, organization, address, gender, email, phone, service_offer,
                                     coordinates, amount, region, years_valid, is_test_vocher, description, footnote)
-        form_show_voucher.show_voucher(voucher)
+        win['form_show_voucher'].show_voucher(voucher)
         self.initialized = False # to load values from profile on next show
         self.close()
-
 
     def init_values(self):
         address = (f"{user_profile.person_data['street']} {user_profile.person_data['zip_code']} "
@@ -476,7 +499,6 @@ class Dialog_Create_Minuto(QMainWindow, Ui_DialogCreateMinuto):
         self.textEdit_service_offer.setText(user_profile.person_data['service_offer'])
         self.label_footnote.setText("Gutschein-Nutzung nur für Mitspieler/innen.")
         self.setWindowTitle(f"eMinuto erstellen - Profil: {user_profile.profile_name}")
-
 
     def update_voucher_description(self, amount):
         """Update the voucher description based on the amount."""
@@ -508,6 +530,7 @@ class DialogVoucherList(QMainWindow, Ui_DialogVoucherList):
     def init_show(self):
         self.init_values()
         self.show()
+        self.raise_()
 
     def init_values(self):
         self.setWindowTitle(f"eMinuto-Liste - Profil: {user_profile.profile_name}")
@@ -590,9 +613,7 @@ class DialogVoucherList(QMainWindow, Ui_DialogVoucherList):
         mapped_index = self.proxyModel.mapToSource(index)
         row = mapped_index.row()
         voucher = self.voucher_mapping[row]
-        form_show_voucher.show_voucher(voucher)
-
-
+        win['form_show_voucher'].show_voucher(voucher)
 
 
 class Dialog_Profile_Login(QMainWindow, Ui_DialogProfileLogin):
@@ -649,6 +670,7 @@ class Dialog_Profile(QMainWindow, Ui_Form_Profile):
         self.textEdit_service_offer.setText(user_profile.person_data['service_offer'])
         self.lineEdit_coordinates.setText(user_profile.person_data['coordinates'])
         self.show()
+        self.raise_()
 
     def save_profile(self):
         user_profile.profile_name = self.lineEdit_profile_name.text()
@@ -665,7 +687,7 @@ class Dialog_Profile(QMainWindow, Ui_Form_Profile):
         user_profile.person_data['phone'] = self.lineEdit_phone.text()
         user_profile.person_data['service_offer'] = self.textEdit_service_offer.toPlainText()
         user_profile.person_data['coordinates'] = self.lineEdit_coordinates.text()
-        user_profile.person.set_person_data(user_profile.person_data) # update person from person_data in profile
+        user_profile.person.set_person_data(user_profile.person_data)  # update person from person_data in profile
         frm_main_window.update_values()
         user_profile.save_profile_to_disk()
 
@@ -687,7 +709,6 @@ class Dialog_Profile_Create_Selection(QMainWindow, Ui_Dialog_Profile_Create_Sele
         self.close()
 
 
-
 class Frm_Mainwin(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
@@ -697,11 +718,11 @@ class Frm_Mainwin(QMainWindow, Ui_MainWindow):
 
         self.setWindowTitle(f"eMinuto")
         self.actionCreateProfile.triggered.connect(self.dialog_generate_profile.show)
-        self.actionEditProfile.triggered.connect(dialog_profile.init_and_show)
-        self.actionCreateMinuto.triggered.connect(dialog_create_minuto.show)
-        self.actionProfileLogin.triggered.connect(dialog_profile_login.login)
+        self.actionEditProfile.triggered.connect(win['dialog_profile'].init_and_show)
+        self.actionCreateMinuto.triggered.connect(win['dialog_create_minuto'].show)
+        self.actionProfileLogin.triggered.connect(win['dialog_profile_login'].login)
         self.actionProfileLogout.triggered.connect(self.profile_logout)
-        self.actionVoucherList.triggered.connect(dialog_voucher_list.init_show)
+        self.actionVoucherList.triggered.connect(win['dialog_voucher_list'].init_show)
         self.actionOpenFile.triggered.connect(open_data_file)
 
         self.actionClose.triggered.connect(self.close)
@@ -709,13 +730,23 @@ class Frm_Mainwin(QMainWindow, Ui_MainWindow):
 
         self.pushButton_copy_user_ID.clicked.connect(self.copyUserIDToClipboard)
 
+    def closeEvent(self, event):
+        # close all windows on close of main win
+        for window in win.values():
+            window.close()
+
+        # Beenden der Anwendung
+        QApplication.instance().quit()
+
+        super().closeEvent(event)
+
     def copyUserIDToClipboard(self):
         clipboard = QApplication.clipboard()
         clipboard.setText(user_profile.person.id)
 
     def onProfileCreated(self):
         self.profile_logout()
-        dialog_profile_login.show()
+        win['dialog_profile_login'].show()
 
     def update_values(self):
         self.setWindowTitle(f"eMinuto - Profil: {user_profile.profile_name}")
@@ -723,7 +754,6 @@ class Frm_Mainwin(QMainWindow, Ui_MainWindow):
         self.label_username.setText(f"{user_profile.person_data['first_name']} {user_profile.person_data['last_name']}")
         self.lineEdit_own_balance.setText(user_profile.get_own_minuto_balance())
         self.lineEdit_other_balance.setText(user_profile.get_other_minuto_balance())
-
 
     def profile_login(self):
         self.update_values()
@@ -765,8 +795,7 @@ class Frm_Mainwin(QMainWindow, Ui_MainWindow):
         profile_name = user_profile.profile_name
         window_title = f"eMinuto"
 
-
-        def hide(object): #helper
+        def hide(object):  # helper
             object.setVisible(False)
 
         def show(object):
@@ -778,8 +807,7 @@ class Frm_Mainwin(QMainWindow, Ui_MainWindow):
         def enable(object):
             object.setEnabled(True)
 
-
-        #changings of GUI
+        # changings of GUI
         if profile_initialized:
             show(self.actionEditProfile)
             show(self.actionProfileLogout)
@@ -791,7 +819,6 @@ class Frm_Mainwin(QMainWindow, Ui_MainWindow):
             hide(self.actionProfileLogin)
             hide(self.actionCreateProfile)
 
-
         if profile_exists and not profile_initialized:
             show(self.actionProfileLogin)
 
@@ -801,11 +828,8 @@ class Frm_Mainwin(QMainWindow, Ui_MainWindow):
             hide(self.actionCreateMinuto)
             hide(self.actionVoucherList)
 
-
-            # disable(self.menuMinuto) does not work
             disable(self.lineEdit_own_balance)
             disable(self.lineEdit_other_balance)
-
 
         if not profile_exists:
             show(self.actionCreateProfile)
@@ -828,16 +852,17 @@ class Frm_Mainwin(QMainWindow, Ui_MainWindow):
 app = QApplication([])
 apply_global_styles(app)
 
-form_sing_as_guarantor = FormSignAsGuarantor()
-form_send_to_guarantor = FormSendToGuarantor()
-form_show_raw_data = FormShowRawData()
-dialog_profile_login = Dialog_Profile_Login()
-dialog_profile_create_selection = Dialog_Profile_Create_Selection()
-dialog_profile = Dialog_Profile()
-dialog_create_minuto = Dialog_Create_Minuto()
-dialog_voucher_list = DialogVoucherList()
-form_show_voucher = FormShowVoucher()
-
+# dict of all windwos
+win = {
+    'form_sign_as_guarantor': FormSignAsGuarantor(),
+    'form_send_to_guarantor': FormSendToGuarantor(),
+    'form_show_raw_data': FormShowRawData(),
+    'dialog_profile_login': Dialog_Profile_Login(),
+    'dialog_profile_create_selection': Dialog_Profile_Create_Selection(),
+    'dialog_profile': Dialog_Profile(),
+    'dialog_create_minuto': Dialog_Create_Minuto(),
+    'dialog_voucher_list': DialogVoucherList(),
+    'form_show_voucher': FormShowVoucher()
+}
 
 frm_main_window = Frm_Mainwin()
-
